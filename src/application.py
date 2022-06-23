a0c96cf6f9ba488282d4361b53a78a81bda6920b
Sam Parker <s4m.prkr@gmail.com>
2022-06-23 20:11:22 +0100

    Timer working and update rate can be varied

--- src/application.py
+++ src/application.py
@@ -1,3 +1,4 @@
+from PyQt6.QtCore import QTimer
 import PyQt6.QtWidgets as qt
 import cv2 as cv
 
@@ -16,38 +17,61 @@ class Window(qt.QWidget):
         self.position_widgets()
         self.connect_widgets()
 
+        # Create a variable to track how many images have been taken so far
+        self.images_taken = 0
+
     def create_widgets(self):
         # Create the form elements
-        self.imageCount = qt.QSpinBox(parent=self)
-        self.imageCount.setRange(1, 250)
+        self.images = qt.QSpinBox(parent=self)
+        self.images.setRange(1, 500)
+        self.images.setSingleStep(10)
+
+        self.refresh = qt.QSpinBox(parent=self)
+        self.refresh.setRange(1, 100)
+
+        self.submit = qt.QPushButton(parent=self)
+        self.submit.setText('Submit')
+
+        self.progress = qt.QProgressBar(parent=self)
+        self.progress.setRange(0, 100)
+
+        self.timer = QTimer(parent=self)
 
     def position_widgets(self):
-        # Create the main window layout
         layout = qt.QHBoxLayout()
-
-        # Create the form layout
+        control = qt.QVBoxLayout()
         form = qt.QFormLayout()
 
-        # Insert here
-        form.addRow('Images (#)', self.imageCount)
+        form.addRow('Images (#)', self.images)
+        form.addRow('Refresh Rate (#/s)', self.refresh)
+        form.addRow(self.submit)
 
-        layout.addLayout(form)
+        control.addLayout(form)
+        control.addWidget(self.progress)
+        layout.addLayout(control)
         self.setLayout(layout)
 
     def connect_widgets(self):
-        pass
+        self.submit.clicked.connect(self.run)
+        self.timer.timeout.connect(self.take_image)
 
+    def run(self):
+        self.images_taken = 0
+        self.timer.setInterval(int(1000/self.refresh.value()))
+        self.progress.setValue(0)
+        self.timer.start()
 
+    def take_image(self):
+        if self.images_taken < self.images.value():
+            print(f'Image {self.images_taken}')
+            self.images_taken += 1
+        else:
+            self.timer.stop()
 
-
-
-
-
-
-
-
-
-
+        int_images_taken = int(self.images_taken / self.images.value() * 100)
+        if int_images_taken > 100: int_images_taken = 100
+        self.progress.setValue(int_images_taken)
+        
 if __name__ == '__main__':
     app = qt.QApplication([])
     window = Window()
