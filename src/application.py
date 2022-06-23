d06414ecb0af03bc867834eed50fafd10cbbee68
Sam Parker <s4m.prkr@gmail.com>
2022-06-23 20:39:50 +0100

    Added directory selection

--- src/application.py
+++ src/application.py
@@ -3,10 +3,11 @@ import PyQt6.QtWidgets as qt
 import cv2 as cv
 
 from sys import exit
+from os import getcwd
 
 class Window(qt.QWidget):
 
-    def __init__(self, size=(300, 200)):
+    def __init__(self, size=(400, 200)):
         # Basic window setup
         super().__init__()
         self.setWindowTitle('Image Capture Tool')
@@ -29,6 +30,13 @@ class Window(qt.QWidget):
         self.refresh = qt.QSpinBox(parent=self)
         self.refresh.setRange(1, 100)
 
+        self.activeDirectory = qt.QLineEdit(parent=self)
+        self.activeDirectory.setText(getcwd())
+        self.activeDirectory.setDisabled(True)
+
+        self.selectDirectory = qt.QPushButton(parent=self)
+        self.selectDirectory.setText('Select Directory')
+
         self.submit = qt.QPushButton(parent=self)
         self.submit.setText('Submit')
 
@@ -44,6 +52,8 @@ class Window(qt.QWidget):
 
         form.addRow('Images (#)', self.images)
         form.addRow('Refresh Rate (#/s)', self.refresh)
+        form.addRow('Active Directory', self.activeDirectory)
+        form.addRow(self.selectDirectory)
         form.addRow(self.submit)
 
         control.addLayout(form)
@@ -52,6 +62,7 @@ class Window(qt.QWidget):
         self.setLayout(layout)
 
     def connect_widgets(self):
+        self.selectDirectory.clicked.connect(self.select_directory)
         self.submit.clicked.connect(self.run)
         self.timer.timeout.connect(self.take_image)
 
@@ -72,6 +83,13 @@ class Window(qt.QWidget):
         if int_images_taken > 100: int_images_taken = 100
         self.progress.setValue(int_images_taken)
         
+    def select_directory(self):
+        dir = qt.QFileDialog.getExistingDirectoryUrl(parent=self).toLocalFile()
+        print(dir)
+        if len(dir) > 0:
+            self.activeDirectory.setText(dir)
+
+
 if __name__ == '__main__':
     app = qt.QApplication([])
     window = Window()
